1947ffe03859971ee6b1673fbbbbcc287457acf6
muskan <muskanloach984@gmail.com>
2025-03-05 16:15:29 +0530

    fix issues

--- src/config/config.js
+++ src/config/config.js
@@ -3,5 +3,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    dburl: process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/"
+    dburl: process.env.MONGO_CONNECTION_STRING || "mongodb+srv://muskanuser:L7mKXFLoJ3p9mq1I@linktree.jctbp.mongodb.net/?retryWrites=true&w=majority&appName=linktree"
 };
